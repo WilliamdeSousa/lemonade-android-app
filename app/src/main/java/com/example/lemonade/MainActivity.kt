2986cb847f6ca11f0fe3710ba16365b4efd264b5
William de Sousa Costa <37248765+WilliamdeSousa@users.noreply.github.com>
2024-05-12 20:42:11 -0300

    Update MainActivity.kt

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -6,15 +6,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -26,7 +25,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
@@ -110,19 +108,16 @@ fun LemonadeApp() {
             verticalArrangement = Arrangement.Center,
             modifier = Modifier.fillMaxSize()
         ) {
-            Image(
-                painter = painter,
-                contentDescription = contentDescription,
-                modifier = Modifier
-                    .size(200.dp)
-                    .clip(shape = RoundedCornerShape(40.dp))
-                    .background(
-                        color = Color(0xFFD2E7DB),
-                        //color = Color(red = 105, green = 205, blue = 216),
-                        shape = RoundedCornerShape(40.dp)
-                    )
-                    .clickable(onClick = action)
-            )
+            Button(
+                onClick = action,
+                shape = RoundedCornerShape(40.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFD2E7DB))
+            ) {
+                Image(
+                    painter = painter,
+                    contentDescription = contentDescription
+                )
+            }
             Spacer(modifier = Modifier.padding(16.dp))
             Text(
                 text= text,
